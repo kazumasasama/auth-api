0c5c1245fb75566827a2ffb310ac1b6800f76aab
Kaz <takaik@yahoo.com>
2022-03-22 11:48:56 -0400

    create sessions controller & route sessions/create

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  # This create action is responsible for finding the user in the database with the matching email and password, then creating a JSON Web Token using the JWT.encode method from the jwt gem.
+  
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      jwt = JWT.encode(
+        {
+          user_id: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i # the expiration time
+        },
+        Rails.application.credentials.fetch(:secret_key_base), # the secret key
+        "HS256" # the encryption algorithm
+      )
+      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
+    else
+      render json: {}, status: :unauthorized
+    end
+  end
+  
+end
